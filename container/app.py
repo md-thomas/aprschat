d5688c68286f3f2ddf9683e64d22deef8bd984a0
Mike Thomas <mdthomas.home@gmail.com>
2025-07-19 12:39:37 -0400

    mdt: updates to use port 5001

--- container/app.py
+++ container/app.py
@@ -92,4 +92,4 @@ def clear_messages():
     return jsonify({"status": "success", "message": "Message buffer cleared."})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
